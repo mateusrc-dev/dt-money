b97dc5db25c7cda7132cd57320276719af4b4f55
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-02-19 21:38:03 -0300

    update project

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -3,13 +3,13 @@ import { TransactionsContext } from '../contexts/TransactionsContext'
 import { useMemo } from 'react'
 
 export function useSummary() {
-  const transactions = useContextSelector(TransactionsContext, (context) => {
-    return context.transactions
+  const allTransactions = useContextSelector(TransactionsContext, (context) => {
+    return context.allTransactions
   })
   // vamos usar o método reduce -> permite percorrer um array e reduzir esse array a uma nova estrutura de dados - nesse caso vamos converter o array de transactions em um objeto com a seguinte estrutura: {income: 0, outcome: 0, total: 0}
   const summary = useMemo(() => {
     // ao usar useMemo, a variável summary só vai ser recriada quando o transactions mudar, antes ela era recriada toda vez que o useSummary fosse renderizado de novo
-    return transactions.reduce(
+    return allTransactions.reduce(
       (acc, transaction) => {
         // dessa função recebemos como parâmetro: 1. resumo atual (accumulator nada mais é que a estrutura inicial abaixo - vamos fazer todas as operações nele - é o que será retornado no final); 2. no segundo recebemos cada transaction da iteração
         if (transaction.type === 'income') {
@@ -28,7 +28,7 @@ export function useSummary() {
         total: 0,
       },
     ) // passamos como parâmetro do reduce: 1. uma função; 2. a estrutura de dados inicial que vamos começar
-  }, [transactions])
+  }, [allTransactions])
 
   return summary // vamos retornar o summary com o valor retornado pelo reduce
 }
